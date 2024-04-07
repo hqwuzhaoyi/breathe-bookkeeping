686b44093fc5c94eb1495332cc3270c79065e1bc
Prajna <hqwuzhaoyi@gmail.com>
2024-04-08 00:04:49 +0800

    Add Button component and footer container

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Image } from "react-native";
 import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
@@ -8,6 +10,10 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -28,5 +34,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
