67129609480cc30550e9260b754ea831bc846177
Prajna <hqwuzhaoyi@gmail.com>
2024-04-16 00:07:56 +0800

    Add image picker functionality to App.js

--- App.js
+++ App.js
@@ -2,17 +2,40 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Image } from "react-native";
 import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
