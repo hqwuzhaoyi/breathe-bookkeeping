edc7374c4336ff4b5f88661cda9379f1e8846c24
Prajna <hqwuzhaoyi@gmail.com>
2024-04-27 23:20:04 +0800

    feat: add react-native-reusables

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,56 +1,79 @@
-import { Stack, Tabs } from "expo-router";
-import {
-  SplashScreen,
-  // This example uses a basic Layout component, but you can use any Layout.
-  Slot,
-} from "expo-router";
-import { useFonts, Inter_500Medium } from "@expo-google-fonts/inter";
-import { useEffect } from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import {
-  ThemeProvider,
-  DarkTheme,
-  DefaultTheme,
-  useTheme,
-} from "@react-navigation/native";
+import '~/global.css';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Theme, ThemeProvider } from '@react-navigation/native';
+import { SplashScreen, Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '~/lib/constants';
+import { useColorScheme } from '~/lib/useColorScheme';
+import { PortalHost } from '~/components/primitives/portal';
+import { ThemeToggle } from '~/components/ThemeToggle';
+
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  dark: true,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before getting the color scheme.
 SplashScreen.preventAutoHideAsync();
 
-export default function Layout() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter_500Medium,
-  });
+export default function RootLayout() {
+  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
 
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
+  React.useEffect(() => {
+    (async () => {
+      const theme = await AsyncStorage.getItem('theme');
+      if (Platform.OS === 'web') {
+        // Adds the background color to the html element to prevent white background on overscroll.
+        document.documentElement.classList.add('bg-background');
+      }
+      if (!theme) {
+        AsyncStorage.setItem('theme', colorScheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      const colorTheme = theme === 'dark' ? 'dark' : 'light';
+      if (colorTheme !== colorScheme) {
+        setColorScheme(colorTheme);
+
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      setIsColorSchemeLoaded(true);
+    })().finally(() => {
       SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
+    });
+  }, []);
 
-  // Prevent rendering until the font has loaded or an error was returned
-  if (!fontsLoaded && !fontError) {
+  if (!isColorSchemeLoaded) {
     return null;
   }
 
   return (
-    <SafeAreaProvider>
-      <ThemeProvider value={DefaultTheme}>
-        <Stack
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "#f4511e",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+      <Stack>
+        <Stack.Screen
+          name='index'
+          options={{
+            title: 'Starter Base',
+            headerRight: () => <ThemeToggle />,
           }}
-        >
-          {/* Optionally configure static options outside the route.*/}
-          <Stack.Screen name="home" options={{}} />
-        </Stack>
-      </ThemeProvider>
-    </SafeAreaProvider>
+        />
+      </Stack>
+      <PortalHost />
+    </ThemeProvider>
   );
 }
