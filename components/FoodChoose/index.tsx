038c033311fb9f5a5a9ad124b7a8a3e51325c3a3
hqwuzhaoyi <hqwuzhaoyi@gmail.com>
2024-06-21 23:59:36 +0800

    feat: Optimize FoodChoose component for responsive layout

--- components/FoodChoose/index.tsx
+++ components/FoodChoose/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { View, FlatList, Text, StyleSheet, Dimensions } from "react-native";
 import { foodEmojis } from "./foods";
 import { Card, CardTitle } from "../ui/card";
+import clsx from "clsx";
 
-const numColumns = Math.floor(Dimensions.get("window").width / 82);
+const screenWidth = Dimensions.get("window").width;
+const cardWidth = 70; // 假设每个卡片的宽度为70
+const spaceBetween = 10; // 每个卡片之间的间隔为10
+const numColumns = Math.floor(screenWidth / (cardWidth + spaceBetween)); // 计算每行可以放置的卡片数
+const totalPadding =
+  screenWidth -
+  numColumns * (cardWidth + spaceBetween - spaceBetween / numColumns); // 计算总剩余空间
 
 export const EmojiSelector = () => {
   const renderItem = ({ item }: { item: (typeof foodEmojis)[number] }) => (
-    <Card className="p-2 m-1">
+    <Card className={clsx("p-2 m-1", `w-[80px]`)}>
       <View className="text-3xl w-16 h-16 rounded-lg justify-center items-center">
         {item.emoji}
       </View>
@@ -21,6 +28,10 @@ export const EmojiSelector = () => {
       renderItem={renderItem}
       keyExtractor={(item, index) => index.toString()}
       numColumns={numColumns}
+      contentContainerStyle={{
+        paddingHorizontal: totalPadding / 2,
+        justifyContent: "center",
+      }}
     />
   );
 };
